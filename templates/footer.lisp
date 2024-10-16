e68a6374c40f95a3a86d99c0dc39440088f1da60
gnat <natalieconnolly621@gmail.com>
2024-10-15 22:37:04 -0700

    add (two-track-pharmacode (hex "nat")) to footer

--- templates/footer.lisp
+++ templates/footer.lisp
@@ -8,5 +8,6 @@
            (div ()
                 (footnote (:style "margin-top: 5px") "all content, with the exception of 88x31 buttons or unless otherwise noted is created by me and is licensed under a CC BY-NC-SA 4.0 license."))
            (a (:href "https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en")
-              (img (:src "/files/by-nc-sa.png" :alt "license 88x31" :width "88" :height "31")))))
+              (img (:src "/files/by-nc-sa.png" :alt "license 88x31" :width "88" :height "31")))
+           (footnote (:style "margin-top: 5px") "▖▖▖▖▘▖▖▖▖▘▌▖▖▖▘")))
 
